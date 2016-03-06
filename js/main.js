1d487c20382c7d7951689bb9097ea0f2a9fbfba1
cyio <icaner@qq.com>
2016-03-06 13:21:22 +0800

    支持在新窗口中打开

--- js/main.js
+++ js/main.js
@@ -333,6 +333,76 @@ netease.bind = function(){
 		}
 	});
 
+	$("#btnNewWinPopup").click(function(){
+		openPopupInNewWin();
+    return false;
+	});
+
+	var _bg_view = null ;
+	function getBackgroundView() {
+	  if (!_bg_view) {
+	    _bg_view = chrome.extension.getBackgroundPage();
+	    if (!_bg_view) {
+	      var views = chrome.extension.getViews({});
+	      for (var i = 0, l = views.length; i < l; i++) {
+	        var view = views[i];
+	        if (view.theViewName && view.theViewName === 'background') {
+	          _bg_view = view;
+	          break;
+	        }
+	      }
+	    }
+	  }
+	  return _bg_view;
+	}
+
+	function decodeForm(form) {
+	        var list = [];
+	        var nvps = form.split('&');
+	        for (var n = 0; n < nvps.length; ++n) {
+	            var nvp = nvps[n];
+	            if (nvp == '') {
+	                continue;
+	            }
+	            var equals = nvp.indexOf('=');
+	            var name;
+	            var value;
+	            if (equals < 0) {
+	                name = OAuth.decodePercent(nvp);
+	                value = null;
+	            } else {
+	                name = OAuth.decodePercent(nvp.substring(0, equals));
+	                value = OAuth.decodePercent(nvp.substring(equals + 1));
+	            }
+	            list.push([name, value]);
+	        }
+	        return list;
+	    }
+
+	function _getWindowId(callback) {
+	  var params = decodeForm(window.location.search);
+	  if (params.windowId) {
+	    callback(params.windowId);
+	  } else {
+	    chrome.tabs.getSelected(null , function(tab) {
+	      callback(tab.windowId);
+	    });
+	  }
+	}
+
+	function openPopupInNewWin(windowId) {
+	  _getWindowId(function(windowId) {
+	    // initOnUnload();
+	    // var settings = Settings.get();
+	    // var args_str = _get_open_window_args(settings.popupWidth, settings.popupHeight);
+	    window.theViewName = 'not_popup';
+	    var url = 'popup.html?is_new_win=true';
+	    if (windowId) {
+	      url += '&windowId=' + windowId;
+	    }
+	    getBackgroundView().new_win_popup.window = window.open(url, '163music', 'width=320,height=362' );
+	  });
+	}
 	
 };
 
